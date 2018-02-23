6d45620d441a24501a7b4e93b731b1321d27db64
Leo Liu <codetown@163.com>
2018-02-23 17:18:08 +0800

    Import devtools.
    Import devtools and edit "common.css"

--- public/webtools.config.php
+++ public/webtools.config.php
@@ -61,4 +61,4 @@ defined('APPLICATION_ENV') || define('APPLICATION_ENV', getenv('APPLICATION_ENV'
 /**
  * @const PTOOLSPATH The path to the Phalcon Developers Tools.
  */
-defined('PTOOLSPATH') || define('PTOOLSPATH', 'E:\upupw\htdocs\phaldev');
+defined('PTOOLSPATH') || define('PTOOLSPATH', 'E:\probase\devtools');
